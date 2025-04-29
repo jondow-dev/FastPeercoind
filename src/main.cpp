1877d97b22fff53be26f6e4ebb3cb4f170cdb81a
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-04-30 01:31:55 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3128,25 +3128,6 @@ bool LoadBlockIndex()
 {
     GetMessageStart(pchMessageStart);
 
-    if (fTestNet)
-    {
-#ifdef TESTING
-        hashGenesisBlock = uint256("00008d0d88095d31f6dbdbcf80f6e51f71adf2be15740301f5e05cc0f3b2d2c0");
-        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 15);
-        nStakeMinAge = 60 * 60 * 24; // test net min age is 1 day
-        nCoinbaseMaturity = 60;
-        bnInitialHashTarget = CBigNum(~uint256(0) >> 15);
-        nModifierInterval = 60 * 20; // test net modifier interval is 20 minutes
-#else
-        hashGenesisBlock = hashGenesisBlockTestNet;
-        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 28);
-        nStakeMinAge = 60 * 60 * 24; // test net min age is 1 day
-        nCoinbaseMaturity = 60;
-        bnInitialHashTarget = CBigNum(~uint256(0) >> 29);
-        nModifierInterval = 60 * 20; // test net modifier interval is 20 minutes
-#endif
-    }
-
     printf("%s Network: genesis=0x%s nBitsLimit=0x%08x nBitsInitial=0x%08x nStakeMinAge=%d nCoinbaseMaturity=%d nModifierInterval=%d\n",
            "Peercoin", hashGenesisBlock.ToString().substr(0, 20).c_str(), bnProofOfWorkLimit.GetCompact(), bnInitialHashTarget.GetCompact(), nStakeMinAge, nCoinbaseMaturity, nModifierInterval);
 
