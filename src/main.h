0a04b176178181050999f82010a1b9d769c32c4e
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-04-30 12:46:29 +0200

    Revert "Changes to compile on Ubuntu 24.04 LTS"
    This reverts commit 562f0dbfc7728267f7d3e809b775511b0249dad4.

--- src/main.h
+++ src/main.h
@@ -2067,7 +2067,7 @@ public:
 
 struct CBlockIndexWorkComparator
 {
-    bool operator()(CBlockIndex *pa, CBlockIndex *pb) const {
+    bool operator()(CBlockIndex *pa, CBlockIndex *pb) {
         if (pa->nChainTrust > pb->nChainTrust) return false;
         if (pa->nChainTrust < pb->nChainTrust) return true;
 
